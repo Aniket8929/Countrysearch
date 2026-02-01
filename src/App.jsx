39f3fff2587ec20dc1f497fabad0d3b1813ef107
Aniket8929 <aniketkumarrai271@gmail.com>
2026-02-01 16:03:32 +0530

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import CountryDetails from "./pages/CountryDetails"
 import HomeLayout from "./pages/HomeLayout";
-import ErrorPage from "./pages/Errorpage";
+import ErrorPage from "./pages/ErrorPage"
 function App() {
   return (
     <>
